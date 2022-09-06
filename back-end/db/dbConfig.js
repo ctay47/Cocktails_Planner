0a671ed9b94e48164d87dbb5c3d0c5ac4734dfcb
Christine Taylor <ctay47@icloud.com>
2022-09-06 18:00:22 -0400

    heroku deployment

--- back-end/db/dbConfig.js
+++ back-end/db/dbConfig.js
@@ -13,12 +13,12 @@ const cn = DATABASE_URL
   }
   : {
 
-    PG_HOST=ec2-3-224-184-9.compute-1.amazonaws.com
-    PG_PORT=5432
-    PG_DATABASE=d2284aahqrgtnt
-    PG_USER = icoyudxeotdvgb
+    PG_HOST,
+    PG_PORT,
+    PG_DATABASE,
+    PG_USER,
 
-
+  }
 
 const db = pgp(cn);
 
